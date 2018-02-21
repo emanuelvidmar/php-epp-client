dee40f8d6cc1d8394c48750b9aee14bf88863ecc
Emanuel <emanuel@avant.si>
2018-02-21 09:05:46 +0100

    New registry -> Regtons

---
+++ Protocols/EPP/eppExtensions/subreg-1.0/includes.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$this->addExtension('subreg-1.0', 'http://www.subreg.cz/epp/subreg-1.0');
+
+include_once(dirname(__FILE__) . '/eppRequests/regtonsEppRegistrarInfoRequest.php');
+include_once(dirname(__FILE__) . '/eppResponses/regtonsEppRegistrarInfoResponse.php');
+
+$this->addCommandResponse('Metaregistrar\EPP\regtonsEppRegistrarInfoRequest', 'Metaregistrar\EPP\regtonsEppRegistrarInfoResponse');
+$this->addCommandResponse('Metaregistrar\EPP\eppCheckDomainRequest', 'Metaregistrar\EPP\regtonsEppCheckDomainResponse');
+$this->addCommandResponse('Metaregistrar\EPP\regtonsEppPricelistRequest', 'Metaregistrar\EPP\regtonsEppPricelistResponse');
+
+
---
+++ Registries/regtonsEppConnection/eppConnection.php
@@ -0,0 +1,15 @@
+<?php
+namespace Metaregistrar\EPP;
+
+class regtonsEppConnection extends eppConnection {
+
+    public function __construct($logging = false, $settingsfile = null) {
+        parent::__construct($logging, $settingsfile);
+
+        //parent::enableDnssec();
+        parent::setServices(array('urn:ietf:params:xml:ns:domain-1.0' => 'domain', 'urn:ietf:params:xml:ns:contact-1.0' => 'contact', "urn:ietf:params:xml:ns:registrar-info-1.0" => "registrar-info"));
+        parent::useExtension('subreg-1.0');
+
+    }
+
+}
