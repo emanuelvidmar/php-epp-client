7346c8a7096269ea273dcc5dd8db3d00fc859661
Emanuel <emanuel@avant.si>
2018-03-22 10:09:31 +0100

    Regtons responses

--- Protocols/EPP/eppExtensions/order-1.0/includes.php
+++ Protocols/EPP/eppExtensions/order-1.0/includes.php
@@ -1,5 +1,8 @@
 <?php
 
 $this->addCommandResponse('Metaregistrar\EPP\regtonsEppOrderInfoRequest', 'Metaregistrar\EPP\regtonsEppOrderInfoResponse');
-$this->addCommandResponse('Metaregistrar\EPP\eppRenewRequest', 'Metaregistrar\EPP\regtonsEppRenewResponse');
+$this->addCommandResponse('Metaregistrar\EPP\eppRenewRequest', 'Metaregistrar\EPP\regtonsEppOrderResponse');
+$this->addCommandResponse('Metaregistrar\EPP\eppTransferRequest', 'Metaregistrar\EPP\regtonsEppOrderResponse');
+$this->addCommandResponse('Metaregistrar\EPP\eppCreateDomainRequest', 'Metaregistrar\EPP\regtonsEppOrderResponse');
+$this->addCommandResponse('Metaregistrar\EPP\eppUpdateDomainRequest', 'Metaregistrar\EPP\regtonsEppOrderResponse');
 
